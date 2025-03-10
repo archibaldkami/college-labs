da0f83739ab4d319b101d1594119dca0dcafbfb2
archibaldkami <archibaldkami@proton.me>
2025-03-10 17:54:59 +0100

    asd labs 1-3

--- a&d structures/build/CMakeFiles/app.dir/app_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/app.dir/app_autogen/mocs_compilation.cpp.o: \
- /home/archibald/Documents/git/college-labs/a&d\ structures/build/app_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
